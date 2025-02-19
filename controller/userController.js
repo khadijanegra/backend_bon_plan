3b1ee3f6c2d5d864285a68646252d7a24cbe406f
khadijanegra <khadijanegra2@gmail.com>
2025-02-19 10:26:06 +0100

    bycrypte

--- controller/userController.js
+++ controller/userController.js
@@ -1,6 +1,6 @@
 const User = require("../models/user");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const crypto = require("crypto");
 const nodemailer = require("nodemailer");
 const jwt = require("jsonwebtoken");
