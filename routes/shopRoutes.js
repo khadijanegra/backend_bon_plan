cba83103b86ecb18a5d4c7e8b293d49919b5a0dc
khadijanegra <khadijanegra2@gmail.com>
2025-04-03 14:44:48 +0100

    getallshops

--- routes/shopRoutes.js
+++ routes/shopRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const shopController = require('../controller/shopController');
 
 router.post('/', shopController.createShop);
-router.get('/', shopController.getAllShops);
+router.get('/getallshops', shopController.getAllShops);
 router.get('/:id', shopController.getShopById);
 router.put('/:id', shopController.updateShop);
 router.delete('/:id', shopController.deleteShop);
