54387c5360cd5a340144f2dc99e0767728efc61f
khadijanegra <khadijanegra2@gmail.com>
2025-04-07 00:00:51 +0100

    iddd

--- routes/eventroutes.js
+++ routes/eventroutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const EventController = require('../controller/eventcontroller');
 
 router.post('/createvent', EventController.createEvent);
-router.get('/getevent/:event_id', EventController.fetchEventById);  // Utiliser le paramètre :shop_id
+router.get('/getevent/:shop_id', EventController.getEventByShopId);  // Utiliser le paramètre :shop_id
 
 
 module.exports = router;
