734f3b8a0e76c3aac61b0602ad82dde0296e2e8f
khadijanegra <khadijanegra2@gmail.com>
2025-04-09 13:32:24 +0100

    usrrrr

--- routes/dashboardroute.js
+++ routes/dashboardroute.js
@@ -4,5 +4,6 @@ const DashboardController = require("../controller/dashboardcontroller");
 const router = express.Router();
 
 router.get('/satisfaction', DashboardController.getSatisfactionStats);
+router.get('/typeusers',DashboardController.getProportionsVisiteursManagers);
 
 module.exports = router
