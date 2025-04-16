989c4df9dd8dcc22ddd5186ea6dce4ab948151c5
Marambenattallah <maram.benattallah3@gmail.com>
2025-04-16 14:00:07 +0200

    AAA

--- routes/webhook.js
+++ routes/webhook.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const Shop= require("../models/shop"); // ton modèle Mongoose
-const shop = require("../models/shop");
+
 
 router.post("/webhook", async (req, res) => {
   const intent = req.body.queryResult.intent.displayName;
@@ -21,7 +21,7 @@ router.post("/webhook", async (req, res) => {
       }
 
       const hotelsList = hotels
-        .map(shop => `🏨 ${shop.nom} - 📞 ${shop.telephone || "non fourni"}`)
+        .map(Shop => `🏨 ${Shop.nom} - 📞 ${Shop.telephone || "non fourni"}`)
         .join("\n");
 
       return res.json({
